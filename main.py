cb76e94f12495f1bf1de991a87d35b3fee522365
Aflah <72096386+aflah02@users.noreply.github.com>
2021-11-20 01:27:19 +0530

    Created Transformer Block and Self Attention Class

---
+++ main.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+
+class SelfAttention(nn.Module):
+    def __init__(self, embed_size, heads):
+        super(SelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = embed_size//heads
+
+        assert (self.head_dim*heads==embed_size), "Embed Size needs to be Divisible by Heads"
+
+        self.values = nn.Linear(self.head_dim, self.head_dim, bias = False)
+        self.keys = nn.Linear(self.head_dim, self.head_dim, bias = False)
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias = False)
+        self.fc_out = nn.Linear(heads*self.embed_size, embed_size)
+
+    def forward(self, values, keys, query, mask):
+        N = query.shape[0]
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+        """Split Embedding into self.head pieces"""
+        values = values.reshape(N, value_len, self.heads, self.head_dim)
+        keys = keys.reshape(N, key_len, self.heads, self.head_dim)
+        queries = query.reshape(N, query_len, self.heads, self.head_dim)
+
+        energy = torch.einsum("nqhd, nkhd->nhqk", [queries, keys])
+
+        # queries shape: (N, query_len, heads, heads_dim)
+        # keys shape: (N, key_len, heads, heads_dim)
+        # energy shape: (N, heads, query_len, key_len)
+
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, float("-1e20"))
+
+        attention = torch.softmax(energy/(self.embed_size**(1/2)), dim=3)
+
+        out = torch.einsum("nhql, nlhd->nqhd", [attention, values]).reshape(
+            N, query_len, self.heads*self.head_dim
+            )
+
+        # attention shape: (N, heads, query_len, key_len)
+        # values shape: (N, value_len, heads, heads_dim)
+        # (N, query_len, heads, head_dim)
+
+        out = self.fc_out(out)
+        return out
+
+class TransformerBlock(nn.Module):
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+        self.attention = SelfAttention(embed_size=embed_size,heads=heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion*embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion*embed_size, embed_size)
+        )
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, value, key, query, mask):
+        attention = self.attention(value, key, query, mask)
+        x = self.norm1(attention+query)
+        x = self.dropout(x)
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward+x))
+        return out
+
